14875f89eccc24ed670d3d16422560b7837b1080
Vadim Yalovets <vadim.yalovets@percona.com>
2022-08-05 13:38:03 +0300

    PSQLADM-409 add libuuid.so lib to tarball

--- scripts/build_binary.sh
+++ scripts/build_binary.sh
@@ -151,7 +151,7 @@ mkdir "$INSTALLDIR"
     then
 
         cd "$INSTALLDIR"
-        LIBLIST="libgcrypt.so libtasn1.so libgnutls.so"
+        LIBLIST="libgcrypt.so libtasn1.so libgnutls.so libuuid.so"
         DIRLIST="usr/bin lib/private"
 
         LIBPATH=""
