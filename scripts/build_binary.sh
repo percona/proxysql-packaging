1cb9d7f8172b75be4cf3bab8b00b40c088ff8f09
Vadim Yalovets <vadim.yalovets@percona.com>
2024-04-14 18:36:39 +0300

    RM-1376 ProxySQL 2.6.2

--- scripts/build_binary.sh
+++ scripts/build_binary.sh
@@ -243,8 +243,7 @@ mkdir "$INSTALLDIR"
         cd "$WORKDIR"
         ls -la
 
-        $TAR czf "proxysql-$VERSION-$(uname -s)-$(uname -m)$GLIBC_VER.tar.gz" \
-            --owner=0 --group=0 -C "$INSTALLDIR/../" "proxysql-$VERSION-$(uname -s)-$(uname -m)$GLIBC_VER"
+        $TAR czf "proxysql-$VERSION-$(uname -s)-$(uname -m)$GLIBC_VER.tar.gz" --owner=0 --group=0 -C "$INSTALLDIR/../" "proxysql-$VERSION-$(uname -s)-$(uname -m)$GLIBC_VER"
     fi
 
     # Clean up build dir
