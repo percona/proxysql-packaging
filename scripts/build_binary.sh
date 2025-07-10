4720eeb48cace9254dc45cdc1e264aecc18f0a46
Vadim Yalovets <vadim.yalovets@percona.com>
2025-07-10 19:16:51 +0300

    PKG-881 Create new job to prepare ProxySQL(v3) packages

--- scripts/build_binary.sh
+++ scripts/build_binary.sh
@@ -127,7 +127,7 @@ mkdir "$INSTALLDIR"
             fi
             # PSQLADM-322 Add pxc_scheduler_handler into ProxySQL package
             git submodule update --init
-            #sed -i 's|command -v go|command -v bash|g' build_scheduler.sh
+            sed -i '/if \[\[ ! -e \$(command -v go 2> \/dev\/null)  \]\]; then/,/fi/ s/^/#/' build_scheduler.sh
             BINGO=$(command -v go)
             sed -i "s|go build|${BINGO} build|g" build_scheduler.sh
             sed -i "s|go mod|${BINGO} mod|g" build_scheduler.sh
