b888528f760f3844959120f9ed414a475eed5f83
Vadim Yalovets <vadim.yalovets@percona.com>
2026-07-13 11:25:19 +0300

    PXC-4935 Packaging tasks for release - PXC 8.4.10

--- scripts/proxysql_builder.sh
+++ scripts/proxysql_builder.sh
@@ -202,15 +202,15 @@ get_system(){
 install_go() {
     export PATH=$PATH:/usr/bin/go/bin
     if [ x"$ARCH" = "xx86_64" ]; then
-        wget https://go.dev/dl/go1.23.12.linux-amd64.tar.gz
+        wget https://go.dev/dl/go1.26.5.linux-amd64.tar.gz
         rm -rf /usr/bin/go
-        tar -C /usr/bin -xzf go1.23.12.linux-amd64.tar.gz
+        tar -C /usr/bin -xzf go1.26.5.linux-amd64.tar.gz
         #update-alternatives --install /usr/bin/go go /usr/local/go/bin/go 1
         #update-alternatives --set go /usr/local/go/bin/go
     else
-        wget https://go.dev/dl/go1.23.12.linux-arm64.tar.gz
+        wget https://go.dev/dl/go1.26.5.linux-arm64.tar.gz
         rm -rf /usr/bin/go
-        tar -C /usr/bin -xzf go1.23.12.linux-arm64.tar.gz
+        tar -C /usr/bin -xzf go1.26.5.linux-arm64.tar.gz
         #update-alternatives --install /usr/bin/go go /usr/local/go/bin/go 1
         #update-alternatives --set go /usr/local/go/bin/go
     fi
