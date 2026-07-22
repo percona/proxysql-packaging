7d3974b528df03594a6f03627aa5a7aa334c8fb6
Vadim Yalovets <vadim.yalovets@percona.com>
2026-07-23 00:04:12 +0300

    PSQLADM-601 [Bld] Build tasks for ProxySQL 3.0.9-1

--- scripts/proxysql_builder.sh
+++ scripts/proxysql_builder.sh
@@ -741,24 +741,14 @@ build_deb(){
         sed -i "s:amd64:arm64:g" debian/control.systemd
         cat debian/control
     fi
-    if [ $DEBIAN_VERSION = "bionic" -o $DEBIAN_VERSION = "jessie" -o $DEBIAN_VERSION = "focal" -o $DEBIAN_VERSION = "jammy" -o $DEBIAN_VERSION = "buster" -o $DEBIAN_VERSION = "stretch" -o $DEBIAN_VERSION = "artful" -o $DEBIAN_VERSION = "bionic" -o ${DEBIAN_VERSION} = "bullseye" -o ${DEBIAN_VERSION} = "bookworm" -o ${DEBIAN_VERSION} = "noble"  -o ${DEBIAN_VERSION} = "trixie" ]; then
-        mv debian/control.systemd debian/control
-        mv debian/rules.systemd debian/rules    
-    elif [ $DEBIAN_VERSION = "xenial" ] && [[ $VERSION == *2* ]]; then
-        curl https://jenkins.percona.com/downloads/PSQLADM-268/openssl -o tools/openssl
-        mv debian/install.xenial debian/install
-        mv debian/rules.xenial debian/rules
-    fi
+    mv debian/control.systemd debian/control
+    mv debian/rules.systemd debian/rules
     dch -m -D "${DEBIAN_VERSION}" --force-distribution -v "2:${VERSION}-${DEB_RELEASE}.${DEBIAN_VERSION}" 'Update distribution'
     unset $(locale|cut -d= -f1)
-    if [ ${DEBIAN_VERSION} = "focal" -o ${DEBIAN_VERSION} = "jammy" -o ${DEBIAN_VERSION} = "bullseye" -o ${DEBIAN_VERSION} = "bookworm" -o ${DEBIAN_VERSION} = "noble" -o ${DEBIAN_VERSION} = "trixie" ]; then
+    if [ ${DEBIAN_VERSION} = "resolute" -o ${DEBIAN_VERSION} = "jammy" -o ${DEBIAN_VERSION} = "bullseye" -o ${DEBIAN_VERSION} = "bookworm" -o ${DEBIAN_VERSION} = "noble" -o ${DEBIAN_VERSION} = "trixie" ]; then
 	sed -i 's:8:10:' debian/compat
         sed -i 's:, dh-systemd::' debian/control
     fi
-    #if [ ${DEBIAN_VERSION} = "jammy" ]; then
-    #	sed -i 's:8:10:' debian/compat
-    #    sed -i 's:, dh-systemd::' debian/control
-    #fi
     dpkg-buildpackage -rfakeroot -us -uc -b
     mkdir -p $CURDIR/deb
     mkdir -p $WORKDIR/deb
