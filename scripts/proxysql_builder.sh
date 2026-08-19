012e8e06541ea222bb63f18a51dfa49891616699
Vadim Yalovets <vadim.yalovets@percona.com>
2026-08-19 16:46:16 +0300

    PS-11349 Packaging tasks for release - PS 8.4.11-11 (#105)

--- scripts/proxysql_builder.sh
+++ scripts/proxysql_builder.sh
@@ -200,23 +200,26 @@ get_system(){
 }
 
 install_go() {
+    GO_VERSION="1.26.6"
     export PATH=$PATH:/usr/bin/go/bin
     if [ x"$ARCH" = "xx86_64" ]; then
-        wget https://go.dev/dl/go1.26.5.linux-amd64.tar.gz
-        rm -rf /usr/bin/go
-        tar -C /usr/bin -xzf go1.26.5.linux-amd64.tar.gz
-        #update-alternatives --install /usr/bin/go go /usr/local/go/bin/go 1
-        #update-alternatives --set go /usr/local/go/bin/go
+        until wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz; do
+            echo "Service not ready, retrying in 30 seconds..."
+            sleep 30
+        done
+        rm -rf /usr/local/go
+        tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
     else
-        wget https://go.dev/dl/go1.26.5.linux-arm64.tar.gz
-        rm -rf /usr/bin/go
-        tar -C /usr/bin -xzf go1.26.5.linux-arm64.tar.gz
-        #update-alternatives --install /usr/bin/go go /usr/local/go/bin/go 1
-        #update-alternatives --set go /usr/local/go/bin/go
+        until wget https://go.dev/dl/go${GO_VERSION}.linux-arm64.tar.gz; do
+            echo "Service not ready, retrying in 30 seconds..."
+            sleep 30
+        done
+        rm -rf /usr/local/go
+        tar -C /usr/local -xzf go${GO_VERSION}.linux-arm64.tar.gz
     fi
-    export PATH=$PATH:/usr/bin/go/bin
+    update-alternatives --install /usr/bin/go go /usr/local/go/bin/go 1
+    update-alternatives --set go /usr/local/go/bin/go
     go version
-    which go
     whereis go
 }
 
